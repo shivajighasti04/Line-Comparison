1c6fa686ed6dbbc99ea522c27d756a133ad75d53
shivajighasti04 <shivajighasti97@gamil.com>
2022-04-30 21:29:02 +0530

    Welcome Message

---
+++ LineComparison.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.linecomparison;
+/**
+ * 
+ * @author Shivaji
+ *  Welcome message
+ *
+ *
+ */
+
+public class LineComparison {
+	 public static void main(String[] args) {
+
+	        System.out.println("----------------Welcome to Line Comparison Computation Program  !-------------------");
+
+	    }
+	}
+
+
+

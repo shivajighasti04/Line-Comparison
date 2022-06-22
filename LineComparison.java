7cf6d7c082268541d6898d1f9cc18cb78607ab68
shivajighasti04 <101914038+shivajighasti04@users.noreply.github.com>
2022-06-22 21:58:31 +0530

    Update LineComparison.java

--- LineComparison.java
+++ LineComparison.java
@@ -1,19 +1,17 @@
 package com.bridgelabz.linecomparison;
+
 /**
- * 
- * @author Shivaji
- *  Welcome message
+ * Welcome Message
+ *
+ * @auther Shivaji Ghasti 
  *
  *
  */
-
 public class LineComparison {
 	 public static void main(String[] args) {
 
 	        System.out.println("----------------Welcome to Line Comparison Computation Program  !-------------------");
-
-	    }
-	}
-
+	 }
+}
 
 

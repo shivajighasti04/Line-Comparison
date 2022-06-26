58c9a2e316cf81996d29f5faba2835ba978d79d6
shivajighasti04 <101914038+shivajighasti04@users.noreply.github.com>
2022-06-26 20:06:40 +0530

    Update LineComparision.java

--- LineComparision.java
+++ LineComparision.java
@@ -8,21 +8,21 @@ import java.util.Scanner;
  * @author Shivaji Ghasti
  */
 public class LineComparision {
-		public static void main(String[] args) {
-			System.out.println("Welcome to Line Comparison Computation Program");
-			System.out.println();
-			Scanner sc = new Scanner(System.in);
-			System.out.print("Enter the  Co-ordinates of start point ( x1 , y1 ) = ");
-			double startX1 = sc.nextDouble();
-			double startY1 = sc.nextDouble();
-	
-			System.out.print("Enter the  Co-ordinates of end point ( x2 , y2 ) = ");
-			double endX2 = sc.nextDouble();
-			double endY2 = sc.nextDouble();
-	
-			// Calculating Length of line
-			double length = Math.sqrt(Math.pow((endX2 - startX1), 2) + Math.pow((endY2 - startY1), 2));
-			System.out.println();
-			System.out.println("Length of Line = " + length);
-		}
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation Program");
+		System.out.println();
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the  Co-ordinates of start point ( x1 , y1 ) = ");
+		double startX1 = sc.nextDouble();
+		double startY1 = sc.nextDouble();
+
+		System.out.print("Enter the  Co-ordinates of end point ( x2 , y2 ) = ");
+		double endX2 = sc.nextDouble();
+		double endY2 = sc.nextDouble();
+
+		// Calculating Length of line
+		double length = Math.sqrt(Math.pow((endX2 - startX1), 2) + Math.pow((endY2 - startY1), 2));
+		System.out.println();
+		System.out.println("Length of Line = " + length);
+	}
 }
